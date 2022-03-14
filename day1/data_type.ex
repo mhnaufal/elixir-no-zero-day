ace60d4d2904dcd7a608b73aa7c9a395164eeac5
mhnaufal <aal.pirates16@gmail.com>
2022-03-14 09:28:09 +0700

    day 1

---
+++ day1/data_type.ex
@@ -0,0 +1,22 @@
+# Basic data types
+
+# Integer
+IO.puts(313)
+
+int = 22
+IO.puts(int)
+
+# Float
+temperature = 31.4
+IO.puts(temperature)
+
+temperature = temperature + 9
+IO.puts(temperature)
+
+# Boolean
+IO.puts(is_boolean(false))
+
+# Binary, Octal, Hexadecimal
+IO.puts("Binary [0b111] : #{0b111}")
+IO.puts("Octal of [0o666] : #{0o666}")
+IO.puts("Hexa of [0xAAA] : #{0xAAA}")
